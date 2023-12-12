3e4f0f3dee143cb5ada133502bd87e6d8bf09007
Perz <s.ali224@mybvc.ca>
2023-12-11 19:46:04 -0700

    Level Rework

--- Assets/Controller/Scripts/Traps/Spike.cs
+++ Assets/Controller/Scripts/Traps/Spike.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class Spike : MonoBehaviour
 {
     private void Awake()
     {
-        GetComponent<PolygonCollider2D>().isTrigger = true;
+        GetComponent<BoxCollider2D>().isTrigger = true;
     }
     
     private void OnTriggerEnter2D(Collider2D collision)
